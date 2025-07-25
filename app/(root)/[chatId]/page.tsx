fcc26206ec03debd05a127f7f895951e1c967eae
mohamed ashraf <90mohamedashraf09@gmail.com>
2025-07-25 06:30:42 +0200

    params type is promise<>

--- app/(root)/[chatId]/page.tsx
+++ app/(root)/[chatId]/page.tsx
@@ -1,29 +1,29 @@
-// import Chat from '@/components/ui/Chat';
-// import { chats } from '@/constants';
-// import { notFound } from 'next/navigation';
+import Chat from '@/components/ui/Chat';
+import { chats } from '@/constants';
+import { notFound } from 'next/navigation';
+
+export default async function ChatPage({
+  params,
+}: {
+  params: Promise<{ chatId: string }>;
+}) {
+  const { chatId } = await params;
+
+  const chat = chats.find((c) => c.id === chatId);
+  if (!chat) return notFound();
+
+  return <Chat {...chat} />;
+}
+
+// app/(root)/[chatId]/page.tsx
 
 // export default async function ChatPage({
 //   params,
 // }: {
 //   params: { chatId: string };
 // }) {
-//   const { chatId } = params;
-
-//   const chat = chats.find((c) => c.id === chatId);
-//   if (!chat) return notFound();
-
-//   return <Chat {...chat} />;
+//   return <div>Chat ID: {params.chatId}</div>;
 // }
-
-// app/(root)/[chatId]/page.tsx
-
-export default async function ChatPage({
-  params,
-}: {
-  params: { chatId: string };
-}) {
-  return <div>Chat ID: {params.chatId}</div>;
-}
 
 // export default async function ChatPage() {
 //   return <div>Chat ID: {'chatId'}</div>;
